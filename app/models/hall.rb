b9d897bf705ffd95c62023e4ee2a85b9155609a6
Adrian Teaca <adrian.teaca.12@aberdeen.ac.uk>
2014-05-19 19:25:45 +0100

    basics

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -22,11 +22,11 @@ class Hall < ActiveRecord::Base
 	end
 
 	def self.open_spreadsheet(file)
-		case File.extname(file.original_filename)
+		case File.extname(file)
 			when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
 			when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
 			when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
-			else raise "Unknown file type: #{file.original_filename}"
+			else raise "Unknown file type: #{file}"
 		end
 	end
   
--- test/unit/hall_test.rb
+++ test/unit/hall_test.rb
@@ -4,47 +4,52 @@ require_relative '../../app/models/hall'
 class HallTest < ActiveSupport::TestCase
 
 	def test_import_size	# Testing if there is an increase in the database elements after an import
-		blen = Quote.all.size
+		blen = Hall.all.size
 		f = File.new('.\test\unit\test.xls')
-		Quote.import(f)
-		alen = Quote.all.size
+		Hall.import(f)
+		f.close
+		alen = Hall.all.size
 		assert_not_equal(alen,blen,"The database hasn't changed after the import")
 	end
 		
 	def test_import_name	# Testing if the import is valid: does it have an name?
 		f = File.new('.\test\unit\test.xls')
-		Quote.import(f)
-		a = Quote.first.nume
+		Hall.import(f)
+		f.close
+		a = Hall.first.nume
 		b = ""
 		assert_not_equal(a,b,"The imported facility doesn't have a name.")
 	end
 	
 	def test_import_type	# Testing if the import is valid: does it have a type?
 		f = File.new('.\test\unit\test.xls')
-		Quote.import(f)
-		a = Quote.first.tipul
+		Hall.import(f)
+		f.close
+		a = Hall.first.tipul
 		b = ""
 		assert_not_equal(a,b,"The imported facility doesn't have a specified type.")
 	end
 	
 	def test_import_postcode	# Testing if the import is valid: does it have a postcode?
 		f = File.new('.\test\unit\test.xls')
-		Quote.import(f)
-		a = Quote.first.postcode
+		Hall.import(f)
+		f.close
+		a = Hall.first.postcode
 		b = "No postcode"
 		assert_not_equal(a,b,"The imported facility doesn't have a postcode.")
 	end
 	
 	def test_import_image	# Testing if the import is valid: does it have an image?
 		f = File.new('.\test\unit\test.xls')
-		Quote.import(f)
-		a = Quote.first.poza
+		Hall.import(f)
+		f.close
+		a = Hall.first.poza
 		b = "No image"
 		assert_not_equal(a,b,"The imported facility doesn't have an image.")
 	end
 	
 	def test_db	# Testing if the database isn't empty.
-		dblen = Quote.all.size
+		dblen = Hall.all.size
 		assert_not_equal(dblen,0,"The database doesn't read any facilities in it")
 	end
 
